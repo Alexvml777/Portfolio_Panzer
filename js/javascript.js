ade221471717a4f217365afc265e7555ffd42e78
Alex Viana <alex.vml777@gmail.com>
2020-07-04 10:56:56 -0300

    Deixando o Menu responsivo, junto a Header

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,8 @@
-function name(params) {
-    
+//Função Menu de Navegação MOBILE
+function openNav(){
+    document.getElementById("mySidenav").style.width = "100%";
 }
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0";
+}
+//Outras funções...
